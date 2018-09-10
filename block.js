bbd2ec146e7e3752443dddc78e96ad7a9e7e7fc1
Alexandr <Voronoy>
2018-09-11 00:52:57 +0300

    init commit

---
+++ block.js
@@ -0,0 +1,17 @@
+class Block {
+    constructor(timestamp, lastHash, hash, data){
+        this.timestamp = timestamp;
+        this.lastHash = lastHash;
+        this.hash = hash;
+        this.data = data;
+    }
+
+    toString(){
+        return `Block - 
+        Timestamp: ${this.timestamp}
+        Last Hash: ${this.lastHash}
+        Hash     : ${this.hash}
+        Data     : ${this.data}
+        `
+    }
+}
