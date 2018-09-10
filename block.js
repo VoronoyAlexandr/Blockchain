a8f63b2df0d1709fddd0a27ad4dc58c389d57154
Alexandr Voronoy <servermed@gmail.com>
2018-09-11 00:56:17 +0300

    Added tests

--- block.js
+++ block.js
@@ -1,17 +1,36 @@
+const SHA256 = require('crypto-js/sha256');
+
 class Block {
-    constructor(timestamp, lastHash, hash, data){
+    constructor(timestamp, lastHash, hash, data) {
         this.timestamp = timestamp;
         this.lastHash = lastHash;
         this.hash = hash;
         this.data = data;
     }
 
-    toString(){
+    toString() {
         return `Block - 
         Timestamp: ${this.timestamp}
-        Last Hash: ${this.lastHash}
-        Hash     : ${this.hash}
-        Data     : ${this.data}
-        `
+        Last Hash: ${this.lastHash.substring(0, 10)}
+        Hash     : ${this.hash.substring(0, 10)}
+        Data     : ${this.data}`;
+    }
+
+    static genesis() {
+        return new this('Genesis time', '-----', 'f1r57-h45h', [])
+    }
+
+    static mineBlock(lastBlock, data) {
+        const timestamp = Date.now();
+        const lastHash = lastBlock.hash;
+        const hash = Block.hash(timestamp, lastHash, data);
+
+        return new this(timestamp, lastHash, hash, data);
+    }
+
+    static hash(timestamp, lastHash, data) {
+        return SHA256(`${timestamp}${lastHash}${data}`).toString();
     }
 }
+
+module.exports = Block;
