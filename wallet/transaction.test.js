cf253007a3baba7aebe2097d1bfb480277dc6191
Alexandr Voronoy <servermed@gmail.com>
2018-09-12 01:17:00 +0300

    Added transaction and tests

---
+++ wallet/transaction.test.js
@@ -0,0 +1,23 @@
+/* global describe it beforeEach expect */
+const Transaction = require('./transaction');
+const Wallet = require('./index');
+
+describe('Transaction', () => {
+    let transaction, wallet, recipient, amount;
+
+    beforeEach(() => {
+        wallet = new Wallet();
+        amount = 50;
+        recipient = 'r3c1p13nt';
+        transaction = Transaction.newTransaction(wallet, recipient, amount);
+    });
+    it('outputs the `amount` subtracted from the wallet balance ', () => {
+        expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount)
+            .toEqual(wallet.balance - amount);
+    });
+
+    it('outputs the `amount` added to the recipient', () => {
+        expect(transaction.outputs.find(output => output.address === recipient).amount)
+            .toEqual(amount);
+    });
+});
