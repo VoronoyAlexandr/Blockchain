c3ae14cb78e51b271223d40270fad4ad9b6a69ed
Alexandr Voronoy <servermed@gmail.com>
2018-09-11 04:33:36 +0300

    Sync chains

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -11,7 +11,9 @@ class P2pServer {
     }
 
     listen() {
-        const server = new Websocket.Server({ port: P2P_PORT });
+        const server = new Websocket.Server({
+            port: P2P_PORT
+        });
         server.on('connection', socket => this.connectSocket(socket));
 
         this.connectToPeers();
@@ -24,12 +26,32 @@ class P2pServer {
             //ws://localhost:5001
             const socket = new Websocket(peer);
             socket.on('open', () => this.connectSocket(socket));
-        })
+        });
     }
 
     connectSocket(socket) {
         this.sockets.push(socket);
         console.log('Socket connected');
+
+        this.messageHandler(socket);
+
+        this.sendChain(socket);
+    }
+
+    messageHandler(socket) {
+        socket.on('message', message => {
+            const data = JSON.parse(message);
+
+            this.blockchain.replaceChain(data);
+        });
+    }
+
+    sendChain(socket) {
+        socket.send(JSON.stringify(this.blockchain.chain));
+    }
+
+    syncChains() {
+        this.sockets.forEach(socket => this.sendChain(socket));
     }
 }
 
